ab4cf24ce32ec67fb92eb5e2e3265eb3312719ea
Andrea <mansitos97@gmail.com>
2023-04-13 14:17:00 +0200

    asd

--- code-to-pep8.py
+++ code-to-pep8.py
@@ -9,4 +9,6 @@ def long_function_name(var_one, var_two, var_three, var_four):
 
 foo = long_function_name(1, 2, 3, 4)
 
-foo = long_function_name(1, 321321, 3, 4)
+foo = long_function_name(1,
+                         
+                         321321, 3, 4)
