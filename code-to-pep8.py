c66fb01ad6b177afec75b5c37f2a8366b6443b0b
Mansitos <mansitos97@gmail.com>
2023-04-13 14:54:17 +0200

    g

--- code-to-pep8.py
+++ code-to-pep8.py
@@ -7,6 +7,10 @@ def long_function_name(var_one, var_two, var_three, var_four):
     # commento bruttino
 
 
-foo = long_function_name(1, 2, 3, 4)
+foo = long_function_name(1,
+                         
+                         2, 3, 4)
 
-foo = long_function_name(1, 321321, 3, 4)
+foo = long_function_name(1,
+                         
+                         321321, 3, 4)
