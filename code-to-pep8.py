589acbc2c7b5d7a286e303cfc4c616f82f0ebaf6
Mansitos <mansitos97@gmail.com>
2023-04-13 14:23:16 +0200

    Commit con codice non PEP8 compliant

--- code-to-pep8.py
+++ code-to-pep8.py
@@ -1,14 +1,18 @@
 # Write some code...
 
 
-def long_function_name(var_one, var_two, var_three, var_four):
+def long_function_name(var_one,
+                        var_two,
+                        var_three, var_four):
     print(var_one)
 
-    # commento bruttino
+             # commento bruttino
 
 
-foo = long_function_name(1, 2, 3, 4)
+foo = long_function_name(1, 
+                         2, 3,
+                           4)
 
 foo = long_function_name(1,
-                         
-                         321321, 3, 4)
+                         321321, 3,
+                         4)
