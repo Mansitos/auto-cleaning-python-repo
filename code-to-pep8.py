4199434cb40a799eae6470cff85523fdbcfab527
Daniele <dany.passabi@me.com>
2023-04-17 16:36:50 +0200

    Ugly code

--- code-to-pep8.py
+++ code-to-pep8.py
@@ -2,11 +2,17 @@
 
 
 def long_function_name(var_one, var_two, var_three, var_four):
-    print(var_one)
+    
+    
+    
+    print(var_one)       
 
     # commento bruttino
 
 
-foo = long_function_name(1, 2, 3, 45)
+foo = long_function_name(
+    1, 2, 3, 
+    45)
 
-foo = long_function_name(1, 321321, 3, 4)
+foo = long_function_name(1,
+                         321321, 3, 4)
